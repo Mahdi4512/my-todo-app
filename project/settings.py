e212690928a1dcfb9ff23a2ca39a42463c49cdb6
Mahdi <mahdimoradi7656@gmail.com>
2025-08-08 23:47:30 +0330

    newest for test

--- project/settings.py
+++ project/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-iuzzd92fgh7bzuw$iw-1lm*l#aie-!ee)&7hjlignnesi887yj'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['.onrender.com', '127.0.0.1', 'localhost']
 
