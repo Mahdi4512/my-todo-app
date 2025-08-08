03efd18431910424c007682d86eee00f3cca4672
Mahdi <mahdimoradi7656@gmail.com>
2025-08-09 01:07:02 +0330

    hello

--- app/urls.py
+++ app/urls.py
@@ -7,11 +7,12 @@ from .views import (
     Taskdelete,
     CompleteTaskView,
 )
+
 urlpatterns = [
     path('', Tasklist.as_view(), name='task_list'),
-    path('task/<int:pk>/', Taskdetail.as_view(), name='task_detail'),
-    path('task/new/', Tasknew.as_view(), name='task_create'),
-    path('task/<int:pk>/edit/', Taskupdate.as_view(), name='task_update'),
-    path('task/<int:pk>/delete/', Taskdelete.as_view(), name='task_delete'),
-    path('task/<int:pk>/complete/', CompleteTaskView.as_view(), name='task_complete'),
+    path('<int:pk>/', Taskdetail.as_view(), name='task_detail'),
+    path('new/', Tasknew.as_view(), name='task_create'),
+    path('<int:pk>/edit/', Taskupdate.as_view(), name='task_update'),
+    path('<int:pk>/delete/', Taskdelete.as_view(), name='task_delete'),
+    path('<int:pk>/complete/', CompleteTaskView.as_view(), name='task_complete'),
 ]
